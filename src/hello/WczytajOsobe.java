2b6708cb817ccd6380e49b11aa0da4f50f72aa8c
Patryk Szopa <patrykszopa@gmail.com>
2018-02-25 11:17:26 +0100

    Dodany komentarz do klasy WczytajOsobe

--- src/hello/WczytajOsobe.java
+++ src/hello/WczytajOsobe.java
@@ -5,6 +5,8 @@ import javax.swing.JOptionPane;
 public class WczytajOsobe {
 	
 	public static Osoba nowaOsoba() {
+		
+		//ta metoda wczytuja nowa osobe na podstawie okna dialogowego
 		String imię = JOptionPane.showInputDialog("Jak masz na imię?");
 		String nazwisko = JOptionPane.showInputDialog("Jak masz na nazwisko?");
 		return new Osoba(imię, nazwisko);
